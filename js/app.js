27420cbe58a61f927485c6988b9134a698dbcab5
Asif Ahmad <asif.zoma.ahmad@gmail.com>
2025-03-27 14:06:50 +0000

    update to slider
    slider styling and html for sliders updated - divs allign. However spacing issue on homepage

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.hero-slider').slick({
+      dots: true,
+      infinite: true,
+    //   autoplay: true,
+      autoplaySpeed: 4000,
+      speed: 600,
+      arrows: false,
+      fade: true,
+      cssEase: 'linear'
+    });
+  });
+  
