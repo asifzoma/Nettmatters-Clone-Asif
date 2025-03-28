a3bcf95be13a0e6c7e542492e717f39470e669ce
Asif Ahmad <asif.zoma.ahmad@gmail.com>
2025-03-28 14:50:14 +0000

    sidebar and website left shift working
    sidebar and website left shift working
    struggling to get the x above the overlay will retry with z index

--- js/app.js
+++ js/app.js
@@ -1,13 +1,33 @@
+// Slider slides the images on the home page
+
 $(document).ready(function () {
     $('.hero-slider').slick({
       dots: true,
       infinite: true,
       autoplay: true,
-      autoplaySpeed: 4000,
+      autoplaySpeed: 3000,
       speed: 600,
       arrows: false,
       fade: true,
-      cssEase: 'linear'
-    });
+      cssEase: 'linear'    });
   });
   
+
+  // Hamburger Menu
+const hamburger = document.querySelector('.hamburger-menu');
+const body = document.body;
+const overlay = document.querySelector('.overlay');
+
+// Toggle sidebar open/close on hamburger click
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('open');       // animate hamburger to X or back
+  body.classList.toggle('menu-open');       // open or close the sidebar/overlay
+});
+
+// close the menu when clicking the overlay
+overlay.addEventListener('click', () => {
+  hamburger.classList.remove('open');
+  body.classList.remove('menu-open');
+});
+
+
